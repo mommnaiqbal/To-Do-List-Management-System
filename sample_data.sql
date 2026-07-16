9a21f585df47e7994e3b7d2dbb1863f35ebffd31
mommnaiqbal <unicornarmo@gmail.com>
2026-07-16 14:12:53 +0500

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO tasks (title, description, due_date, priority, status)
+VALUES
+('Complete Assignment',
+ 'Finish SQL project before Friday',
+ '2026-07-20',
+ 'High',
+ 'Pending'),
+
+('Buy Groceries',
+ 'Milk, Eggs, Bread',
+ '2026-07-18',
+ 'Medium',
+ 'Completed'),
+
+('Workout',
+ 'Go to the gym for one hour',
+ '2026-07-17',
+ 'Low',
+ 'Pending');
